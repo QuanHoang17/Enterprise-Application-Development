8fb162688bfac8e16df4ff2230222d3ba7d270c7
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-04-25 18:02:45 +0700

    Apr 25th 18:02
    Finished img selection

---
+++ front-end/js/product-description.js
@@ -0,0 +1,48 @@
+//----------------------------------------- Variables ------------------------------------//
+var kerr1 = '../assets/kerr_1.jpeg';
+var kerr2 = '../assets/kerr_2.jpeg';
+var kerr3 = '../assets/kerr_3.png';
+var kerr4 = '../assets/kerr_4.jpeg';
+var kerr5 = '../assets/kerr_5.jpeg';
+
+var mainPic = document.querySelector(".main-pic");
+var small_1 = document.querySelector(".small-pic-1");
+var small_2 = document.querySelector(".small-pic-2");
+var small_3 = document.querySelector(".small-pic-3");
+var small_4 = document.querySelector(".small-pic-4");
+var small_5 = document.querySelector(".small-pic-5");
+
+var smallPicArr = [
+    [small_1, kerr1], 
+    [small_2, kerr2],
+    [small_3, kerr3], 
+    [small_4, kerr4],
+    [small_5, kerr5]
+];
+
+//----------------------------------------- Functions ------------------------------------//
+
+//Reset grayscale and opacity of all small pics
+function resetSmalls(params) {
+    smallPicArr.forEach(element => {
+        element[0].style.cssText = "opacity: 0.5; filter: grayscale(100%)";
+    });
+}
+
+/*
+Default state of main pic and relevant small pic
+*/
+small_1.style.cssText = "opacity: 1; filter: grayscale(0%)";
+
+/* 
+Control the change of main pic
+*/
+smallPicArr.forEach(element => {
+    element[0].addEventListener("click", function changeMainPic(params) {
+        resetSmalls();
+        mainPic.src = element[1];
+        element[0].style.cssText = "opacity: 1; filter: grayscale(0%)";
+    })
+})
+
+
