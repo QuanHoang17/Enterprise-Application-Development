46e9dcf5bd57ee0cc1e4f03a450c5bfef6a5d31c
Hoang Nguyen <hoangdesu@gmail.com>
2021-04-27 12:35:43 +0700

    new products

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,9 +1,18 @@
 // Reusable components
 
 const header = document.querySelector('#header');
+const footer = document.querySelector('#footer');
+
 fetch('./components/Header/header.html')
     .then(res => res.text())
     .then(data => {
         header.innerHTML = data;
+    })
+    .catch(e => console.log(e))
+
+fetch('./components/Footer/footer.html')
+    .then(res => res.text())
+    .then(data => {
+        footer.innerHTML = data;
     })
     .catch(e => console.log(e))
