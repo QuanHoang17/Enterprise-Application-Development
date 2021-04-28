bf49d096029407618d5d5f5adb2cbd608c5a1fd9
dtnLong <49140143+dtnLong@users.noreply.github.com>
2021-04-28 22:43:19 +0700

    Add password encryption

--- back-end/GeaRMIT/src/main/java/com/group5/gearmit/dao/UserDAO.java
+++ back-end/GeaRMIT/src/main/java/com/group5/gearmit/dao/UserDAO.java
@@ -4,7 +4,9 @@ import com.group5.gearmit.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDAO extends JpaRepository<Users, Integer> {
     @Query(value = "select u from Users u where u.email = :email")
     Users getUsersByEmail(@Param("email") String email);
--- back-end/GeaRMIT/src/main/java/com/group5/gearmit/service/UserServiceI.java
+++ back-end/GeaRMIT/src/main/java/com/group5/gearmit/service/UserServiceI.java
@@ -3,41 +3,51 @@ package com.group5.gearmit.service;
 import com.group5.gearmit.dao.UserDAO;
 import com.group5.gearmit.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Transactional
 public class UserServiceI implements UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private UserDAO userDAO;
 
     @Override
+    @Transactional
     public boolean checkName(String name) {
         Users user = userDAO.getUsersByName(name);
         return user != null;
     }
 
     @Override
+    @Transactional
     public boolean checkEmail(String email) {
         Users user = userDAO.getUsersByEmail(email);
         return user != null;
     }
 
     @Override
+    @Transactional
     public String loginUser(Map<String, String> user) {
         String storedPassword = userDAO.getUsersPasswordByName(user.get("name"));
         if (storedPassword == null) {
             return "name";
         }
-        if (!storedPassword.equals(user.get("password"))) {
+        if (!passwordEncoder.matches(user.get("password"), storedPassword)) {
             return "password";
         }
         return "success";
     }
 
     @Override
+    @Transactional
     public Map<String, String> addUser(Map<String, String> userInfo) {
         Map<String, String> response = new HashMap<String, String>();
         boolean nameExisted = checkName(userInfo.get("name"));
@@ -58,7 +68,7 @@ public class UserServiceI implements UserService {
             Users user = new Users();
             user.setName(userInfo.get("name"));
             user.setEmail(userInfo.get("email"));
-            user.setPassword(userInfo.get("password"));
+            user.setPassword(passwordEncoder.encode(userInfo.get("password")));
             user.setPhone(userInfo.get("phone"));
             userDAO.saveAndFlush(user);
             response.put("status", "success");
