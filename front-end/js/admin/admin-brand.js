9a262d75825753844711b2a525c4bce6d0a2c579
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-05-08 17:32:45 +0700

    Finish admin - brand

--- front-end/js/admin/admin-brand.js
+++ front-end/js/admin/admin-brand.js
@@ -1,25 +1,20 @@
-var modalContainer = document.querySelector(".brand-delete-modal-container");
-var deleteTrigger = document.querySelector(".brand-delete-modal-trigger");
-var exitBtn = document.querySelector(".brand-delete-exit");
-var deleteBtn = document.querySelector(".brand-delete-button");
-var modalSearchBtn = document.querySelector(".brand-delete-modal-search");
+var modalContainer = document.querySelector(".brand-add-modal-container");
+var deleteTrigger = document.querySelector(".brand-add-modal-trigger");
+var exitBtn = document.querySelector(".brand-add-exit");
+var addBtn = document.querySelector(".brand-add-button");
 
 deleteTrigger.addEventListener("click", (e) => {
     modalContainer.style.cssText = "display: block";
 })
 
-deleteBtn.addEventListener("click", (e) => {
-    alert('Hello World!');
+addBtn.addEventListener("click", (e) => {
+    let brandName = document.querySelector(".brand-add-modal-input").value;
+    alert(brandName);
 })
 
 exitBtn.addEventListener("click", (e) => {
     modalContainer.style.cssText = "display: none";
 
     // Clear input value when exiting the modal
-    document.querySelector(".brand-delete-modal-input").value = "";
-})
-
-modalSearchBtn.addEventListener("click", (e) => {
-    let customerName = document.querySelector(".brand-delete-modal-input").value;
-    alert(customerName);
+    document.querySelector(".brand-add-modal-input").value = "";
 })
