46eda2146e4ad38c29db879b9af9219c6b0c0988
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-05-12 18:21:32 +0700

    Display brands from db to frontend

--- back-end/GeaRMIT/src/main/java/com/group5/gearmit/controller/BrandController.java
+++ back-end/GeaRMIT/src/main/java/com/group5/gearmit/controller/BrandController.java
@@ -3,11 +3,9 @@ package com.group5.gearmit.controller;
 import com.group5.gearmit.entity.Brand;
 import com.group5.gearmit.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,5 +17,10 @@ public class BrandController {
     @PostMapping (value = "/api/brand")
     public Map<String, String> addBrand(@RequestBody Map<String, String> brand) {
         return brandService.addBrand(brand);
+    }
+
+    @GetMapping(value = "/api/brand")
+    public List<Brand> getAllBrands(){
+        return brandService.getAllBrand();
     }
 }
