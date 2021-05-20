f7d7cc6e5796172172df1d766874a0d9409f1d64
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-05-20 16:19:15 +0700

    Finish js for product description

--- front-end/js/product-description.js
+++ front-end/js/product-description.js
@@ -1,68 +1,94 @@
 //----------------------------------------- Variables ------------------------------------//
 
 // For images
-var kerr1 = '../assets/kerr_1.jpeg';
-var kerr2 = '../assets/kerr_2.jpeg';
-var kerr3 = '../assets/kerr_3.png';
-var kerr4 = '../assets/kerr_4.jpeg';
-var kerr5 = '../assets/kerr_5.jpeg';
-
 var mainPic = document.querySelector(".main-pic");
-var small_1 = document.querySelector(".small-pic-1");
-var small_2 = document.querySelector(".small-pic-2");
-var small_3 = document.querySelector(".small-pic-3");
-var small_4 = document.querySelector(".small-pic-4");
-var small_5 = document.querySelector(".small-pic-5");
-
-var smallPicArr = [
-    [small_1, kerr1], 
-    [small_2, kerr2],
-    [small_3, kerr3], 
-    [small_4, kerr4],
-    [small_5, kerr5]
-];
+var productName = document.querySelector(".product-name");
+var productCategory = document.querySelector(".product-category");
+var productStatus = document.querySelector(".product-status");
+var productPrice = document.querySelector(".product-price");
+var productDescription = document.querySelector(".description");
+var imgList = [];
+var colorList = [];
 
 //----------------------------------------- Functions ------------------------------------//
 
-//Reset grayscale and opacity of all small pics
-function resetSmalls(params) {
-    smallPicArr.forEach(element => {
-        element[0].style.cssText = "opacity: 0.5; filter: grayscale(100%)";
-    });
-}
+// Fetch product information from product name in url
+const itemName = new URLSearchParams(window.location.search).get('name');
+console.log(itemName);
 
-//Default state of main pic and relevant small pic    
-small_1.style.cssText = "opacity: 1; filter: grayscale(0%)";
+fetch(`http://localhost:8080/api/product/name/${itemName}`)
+    .then(response => response.json())
+    .then(product => {
+        console.log(product);
+
+        productName.innerHTML = product[0].name;
+        productCategory.innerHTML = product[0].categoryName;
+        productPrice.innerHTML = product[0].price + ' USD';
+        productDescription.innerHTML = product[0].description;
+        imgList = product[0].imageName;
+        colorList = product[0].color;
 
 
-//Control the change of main pic when small pic is selected
-smallPicArr.forEach(element => {
-    element[0].addEventListener("click", function changeMainPic(params) {
-        resetSmalls();
-        mainPic.src = element[1];
-        element[0].style.cssText = "opacity: 1; filter: grayscale(0%)";
+
+        // Update color choices
+        colorList.forEach(color => {
+            document.querySelector(".color").innerHTML +=
+            `
+            <div class="choice">
+                <i class="fas fa-check-circle"></i>
+                <span class="">${color}</span>
+            </div>
+            `;
+        })
+
+
+
+        // Controller for choices
+        const colorChoiceList = document.querySelectorAll('.choice');
+        Array.from(colorChoiceList).forEach(colorChoice => {
+            colorChoice.addEventListener('click', (e) => {
+                document.querySelector('.active').classList.remove('active');
+
+                colorChoice.classList.add('active');
+            })
+        })
+
+
+
+        // Controller for images
+        // Change image for main pic and first small pic
+        mainPic.src = `http://localhost:8080/api/image/${imgList[0]}`;
+
+        document.querySelector(".small-pics").innerHTML +=
+        `
+        <div class="small-pic-container">
+            <div class="img-container">
+                <img src="http://localhost:8080/api/image/${imgList[0]}" alt="" class="small-pic img-active">
+            </div>
+        </div>
+        `;
+
+        // Change image for remaining small pics
+        for (let i = 1; i < imgList.length; i++) {
+            document.querySelector(".small-pics").innerHTML +=
+            `
+            <div class="small-pic-container">
+                <div class="img-container">
+                    <img src="http://localhost:8080/api/image/${imgList[i]}" alt="" class="small-pic">
+                </div>
+            </div>
+            `;
+        }
+
+        // Click actions for small pics
+        const smallPicList = document.querySelectorAll('.small-pic');
+        Array.from(smallPicList).forEach(smallPic => {
+            smallPic.addEventListener('click', (e) => {
+                document.querySelector('.img-active').classList.remove('img-active');
+
+                smallPic.classList.add('img-active');
+
+                mainPic.src = smallPic.src;
+            })
+        })
     })
-})
-
-// Controller for choices
-const colorChoiceList = document.querySelectorAll('.choice');
-
-Array.from(colorChoiceList).forEach(colorChoice => {
-    colorChoice.addEventListener('click', (e) =>{
-        document.querySelector('.active').classList.remove('active');
-        
-        colorChoice.classList.add('active');
-    })
-})
-
-
-// Test button
-document.querySelector("button").addEventListener("click", function testButton(params) {
-    alert("Button is clicked");
-})
-
-
-// Fetch product ID from url
-const itemId = new URLSearchParams(window.location.search).get('id');
-console.log(itemId);
-
