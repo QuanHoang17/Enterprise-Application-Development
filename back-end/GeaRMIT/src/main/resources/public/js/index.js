cd1043ef1336f08686f7f19b2762f2bb7630860f
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-05-23 09:59:38 +0700

    Fix discount price

--- back-end/GeaRMIT/src/main/resources/public/js/index.js
+++ back-end/GeaRMIT/src/main/resources/public/js/index.js
@@ -1,41 +1,41 @@
 /*------------- FETCH DATA INTO CATEGORY SECTIONS --------------*/
-const productUrl = 'http://localhost:8080/api/product'; 
-let productFetched =[];
+const productUrl = 'http://localhost:8080/api/product';
+let productFetched = [];
 const fetchProductsFromDatabase = async () => {
     const response = await fetch(productUrl);
-   
-   
+
+
     response.json().then((productList) => {
-      productFetched = productList;
-        
-      displayProductByCategory('Headsets');
-      displayNewProduct();
-      loadCarouselProduct();
+        productFetched = productList;
+
+        displayProductByCategory('Headsets');
+        displayNewProduct();
+        loadCarouselProduct();
     })
 }
 
 const displayProductByCategory = (categoryToFilter) => {
-  let cardList = productFetched.filter(({categoryName}) => categoryName === categoryToFilter).map(({name, quantity, categoryName, price, description, imageName, color}) => {
-          let available = 'Outstock';
-          let imgSrc = 'http://localhost:8080/api/image/' + imageName[0]; 
-          quantity > 0 ? available = 'Available' : available = 'Outstock'; 
-          
-          return createProductCardElement(name, description, price.toString(), categoryName, available, color.join(', '), imgSrc);
-        });
-      
-      
-        document.querySelector('.product-detail-list.section').replaceChildren(...cardList);
-      
+    let cardList = productFetched.filter(({ categoryName }) => categoryName === categoryToFilter).map(({ name, quantity, categoryName, price, description, imageName, color }) => {
+        let available = 'Outstock';
+        let imgSrc = 'http://localhost:8080/api/image/' + imageName[0];
+        quantity > 0 ? available = 'Available' : available = 'Outstock';
+
+        return createProductCardElement(name, description, price.toString(), categoryName, available, color.join(', '), imgSrc);
+    });
+
+
+    document.querySelector('.product-detail-list.section').replaceChildren(...cardList);
+
 }
 
 const displayNewProduct = () => {
-  // Sort the products by issueDate in descending order
-  productFetched.sort((a, b) => {if (a.issueDate < b.issueDate) {return 1;} return -1;})
- 
-  const newProductContainer = document.querySelector('.new-product-section');
-  newProductContainer.innerHTML='';
-  for (let i = 0; i < 2; i++){
-      newProductContainer.innerHTML+=`
+    // Sort the products by issueDate in descending order
+    productFetched.sort((a, b) => { if (a.issueDate < b.issueDate) { return 1; } return -1; })
+
+    const newProductContainer = document.querySelector('.new-product-section');
+    newProductContainer.innerHTML = '';
+    for (let i = 0; i < 2; i++) {
+        newProductContainer.innerHTML += `
         <div class="new-product-card">
                     <h1>${productFetched[i].name}</h1>
                     
@@ -45,18 +45,18 @@ const displayNewProduct = () => {
                     
                     
                 </div> `
-    
-  }
-  
+
+    }
+
 }
 
 const createProductCardElement = (name, description, price, category, available, colorList, imgSrc) => {
-  let cardContainer =document.createElement('DIV');
-  cardContainer.classList.add('product-detail-card');
-  
-  
-  cardContainer.innerHTML = `
-  
+    let cardContainer = document.createElement('DIV');
+    cardContainer.classList.add('product-detail-card');
+
+
+    cardContainer.innerHTML = `
+
     <img src= "${imgSrc}" alt=${name}/>
                             <div class="product-info">
                                 <h5>${name}</h5>
@@ -72,7 +72,7 @@ const createProductCardElement = (name, description, price, category, available,
                             </div>
                             <div class="price-and-shipping">
                                 <h1>${price} USD</h1>
-                                <p>99.69 USD</p>
+                                <p>${parseInt(price * 1.3)} USD</p>
                                 <p>Free Shipping</p>
                                 <button>Add to Cart</button>
                                 <a href='./pages/product-description.html?&name=${name}'><button><i class="fas fa-info-circle"></i>
@@ -80,8 +80,8 @@ const createProductCardElement = (name, description, price, category, available,
                             </div>
   
   
-  ` 
-  return cardContainer; 
+  `
+    return cardContainer;
 }
 
 fetchProductsFromDatabase();
@@ -98,13 +98,13 @@ fetchProductsFromDatabase();
 
 
 /*------ Best Seller Carousel Product Fetching------- */
-const carouselList  = document.querySelector('.best-seller-product-list');
+const carouselList = document.querySelector('.best-seller-product-list');
 const NUMBER_OF_ITEMS = 4;
 
 
 const buildCard = (name, description, price, imageName) => {
-            let card = `<div class="best-seller-product-card item">
-            <span class="discount">-69%</span>
+    let card = `<div class="best-seller-product-card item">
+            <span class="discount">-30%</span>
             <img src="http://localhost:8080/api/image/${imageName[0]}"
                 alt="" class="best-seller-product-image">
             <p class="product-title">${name}</p>
@@ -112,7 +112,7 @@ const buildCard = (name, description, price, imageName) => {
             <div class="small-container">
                 <div class="smaller-container">
                     <p class="product-price">${price} USD</p>
-                    <p class="product-old-price">96 USD</p>
+                    <p class="product-old-price">${parseInt(price * 1.3)} USD</p>
                 </div>
 
                
@@ -120,62 +120,62 @@ const buildCard = (name, description, price, imageName) => {
             </div>
         </div>`;
 
-            return card;
-        }
+    return card;
+}
 
 
 
 const loadCarouselProduct = () => {
-  
-
-  // Reset Carousel If There Are Any Products.
-  for (let i = 0; i < NUMBER_OF_ITEMS; i++) {
-     $(".best-seller-product-list").trigger('remove.owl.carousel', [i]).trigger('refresh.owl.carousel');
-  };
 
 
-  for (let i = 0; i < NUMBER_OF_ITEMS; i++){
-    let {name, description, price, imageName} = productFetched[i];
-    let productCard = buildCard(name, description, price, imageName);
+    // Reset Carousel If There Are Any Products.
+    for (let i = 0; i < NUMBER_OF_ITEMS; i++) {
+        $(".best-seller-product-list").trigger('remove.owl.carousel', [i]).trigger('refresh.owl.carousel');
+    };
 
-    
-     $('.best-seller-product-list').owlCarousel('add', productCard).owlCarousel('update');
-    
-     
-  }
 
-  loadCarousel();
-  
+    for (let i = 0; i < NUMBER_OF_ITEMS; i++) {
+        let { name, description, price, imageName } = productFetched[i];
+        let productCard = buildCard(name, description, price, imageName);
+
+
+        $('.best-seller-product-list').owlCarousel('add', productCard).owlCarousel('update');
+
+
+    }
+
+    loadCarousel();
+
 }
 
 
 
 function loadCarousel() {
 
-            $('.best-seller-product-list')
-                .owlCarousel({
+    $('.best-seller-product-list')
+        .owlCarousel({
+            items: 4,
+            dots: true,
+            loop: true,
+            margin: 20,
+            navigation: true,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
                     items: 4,
-                    dots: true,
-                    loop: true,
-                    margin: 20,
-                    navigation: true,
-                    nav: true,
-                    autoplay: true,
-                    autoplayTimeout: 2000,
-                    autoplayHoverPause: true,
-                    responsive: {
-                        0: {
-                            items: 1
-                        },
-                        600: {
-                            items: 3
-                        },
-                        1000: {
-                            items: 4,
-                        }
-                    }
-                })
-        }
+                }
+            }
+        })
+}
 
 
 // Function calls
@@ -189,33 +189,33 @@ $(function () {
 const btn = $('#btnScrollTop');
 
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
 });
 
 btn.on('click', function (e) {
-  e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '300');
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
 });
 
 /*------ Toggle Category Style ------- */
-$(function() {
-        $('ul li').click(function() {
-            $(this).addClass('selected').siblings('li').removeClass('selected');
-            let categoryName = $(this).text();
-            let sectionIndex = parseInt($(this).index() + 1);
-            let currentSection = `.section:nth-of-type(${sectionIndex})`;
-        
-            $(currentSection).stop().fadeIn(200, 'linear').siblings().stop().hide();
-            displayProductByCategory(categoryName);
+$(function () {
+    $('ul li').click(function () {
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+        let categoryName = $(this).text();
+        let sectionIndex = parseInt($(this).index() + 1);
+        let currentSection = `.section:nth-of-type(${sectionIndex})`;
 
-            document.querySelector('#browse-category-cta').setAttribute('href', `./pages/category.html?&category=${categoryName}`);
-            
-        })
+        $(currentSection).stop().fadeIn(200, 'linear').siblings().stop().hide();
+        displayProductByCategory(categoryName);
+
+        document.querySelector('#browse-category-cta').setAttribute('href', `./pages/category.html?&category=${categoryName}`);
+
     })
+})
 
 
 
@@ -223,18 +223,18 @@ $(function() {
 // Select category section
 window.onload = () => {
 
-  // Toggle burger button
-  let menuOpen = false;
-  $('#nav-icon3').click(function () {
-    $(this).toggleClass('open');
-    if (menuOpen === false) {
-      menuOpen = true;
-      $('.browse-category').show();
-    } else {
-      menuOpen = false;
-      $('.browse-category').hide();
-    }
-  });
+    // Toggle burger button
+    let menuOpen = false;
+    $('#nav-icon3').click(function () {
+        $(this).toggleClass('open');
+        if (menuOpen === false) {
+            menuOpen = true;
+            $('.browse-category').show();
+        } else {
+            menuOpen = false;
+            $('.browse-category').hide();
+        }
+    });
 }
 
 
